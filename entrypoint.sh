55c25f3a3ff613aa5fec9e8c7dba8107e5285fde
Gabriel Arama <gabriel@fixter.co.uk>
2020-05-08 18:30:56 +0300

    option to not add release version to s3 path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,19 @@
 set -e
 set -o xtrace
 
-export BRANCH_NAME="${GITHUB_REF#refs/heads/}"
-export DEV_VERSION="$BRANCH_NAME-$(date +'%Y%m%d%H%M')"
-
-if [[ "$BRANCH_NAME" =~ ^[a-zA-Z0-9]\.[a-zA-Z0-9]\.[a-zA-Z0-9]$ ]]; then
-  export RELEASE_VERSION="$BRANCH_NAME"
+if [ "$NO_RELEASE_PATH" = true ] ; then
+  aws s3 cp --acl private $TARGET s3://$BUCKET_NAME/$BUCKET_PATH/
 else
-  export RELEASE_VERSION="$DEV_VERSION"
+  BRANCH_NAME="${GITHUB_REF#refs/heads/}"
+  DEV_VERSION="$BRANCH_NAME-$(date +'%Y%m%d%H%M')"
+
+  if [[ "$BRANCH_NAME" =~ ^[a-zA-Z0-9]\.[a-zA-Z0-9]\.[a-zA-Z0-9]$ ]]; then
+    RELEASE_VERSION="$BRANCH_NAME"
+  else
+    RELEASE_VERSION="$DEV_VERSION"
+  fi
+
+  echo $RELEASE_VERSION
+
+  aws s3 cp --acl private $TARGET s3://$BUCKET_NAME/$BUCKET_PATH/$RELEASE_VERSION/
 fi
-
-echo $RELEASE_VERSION
-
-aws s3 cp --acl private $TARGET s3://$BUCKET_NAME/$BUCKET_PATH/$RELEASE_VERSION/
